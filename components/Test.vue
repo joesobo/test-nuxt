800db0dac7573d4865ac3db3fa3a18001b1dc22f
Joe Soboleski <soboleskijoe@gmail.com>
2021-09-28 16:13:09 -0700

    more examples

--- components/Test.vue
+++ components/Test.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
+    <!-- Should result in `Hello World!` -->
     <div>{{ $t('common.welcome') }}</div>
+    <!-- Should result in `Error` -->
     <div>{{ $tn('common.welcome') }}</div>
+    <!-- Should result in `Another one!` -->
     <div>{{ $tn('.test') }}</div>
+    <!-- Should result in `car` -->
     <div>{{ $tc('common.car', 1) }}</div>
+    <!-- Should result in `cars` -->
     <div>{{ $tc('common.car', 2) }}</div>
   </div>
 </template>
@@ -14,9 +19,13 @@ import Vue from 'vue'
 export default Vue.extend({
   i18nNamespace: 'Test.nested',
   mounted() {
+    // Should result in `This is a test`
     console.log(this.$t('Test.componentTest'))
+    // Should result in `Another one!`
     console.log(this.$t('Test.nested.test'))
+    // Should result in `cars`
     console.log(this.$tc('common.car', 2))
+    // Should result in `Another one!`
     console.log(this.$tn('.test'))
   },
 })
